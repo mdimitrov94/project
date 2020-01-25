e207bb5ec76297bd39f82b65626cb4633980ff8f
mdimitrov94 <martindimitrov944@gmail.com>
2020-01-25 20:56:06 +0200

    Update user.js

--- user.js
+++ user.js
@@ -17,7 +17,7 @@ export const user = {
         post('user', 'login', { username, password }, 'Basic')
             .then(data => {
                 sessionInfo(data)
-                ctx.redirect('/')
+                ctx.redirect('#/')
             })
             .catch(console.error);
     },
